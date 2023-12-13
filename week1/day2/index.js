e9f35b0161e55b65b4391a62bfe96c296de338d2
abhiprd-219 <abhiprd4685@gmail.com>
2023-12-13 17:20:35 +0530

    Tasks of the day

--- week1/day2/index.js
+++ week1/day2/index.js
@@ -117,28 +117,144 @@ const divideNumbers = (num1, num2) => (num2 !== 0) ? num1 / num2 : 'Cannot divid
 
 //rest operstor
 
-function mul(...val){
-    // var m =1;
-    for(let i in val){
-        console.log(i);
-    }
+// function mul(...val){
+//     // var m =1;
+//     for(let i in val){
+//         console.log(i);
+//     }
 
-   // console.log(...val);
+//    // console.log(...val);
 
+
+// }
+
+//  mul(1,2,3,4,5);
+// // console.log(multiply);
+
+// function print(...val) {
+//     for (let i in val) {
+//         console.log(val[i]);
+//     }
+// }
+
+// let val = ['a', 'b', { a: 10 }, 1, 2, 3, 4];
+// print(...val);
+
+let sum = 0;
+function add(val){
+    sum+= val;
+}
+const arr = [10,23,34,45,56];
+ arr.forEach(add);
+
+//console.log("sum" + " = " + sum);
+
+arr.forEach(function(cValue,idx){
+    //console.log(idx+ " : " + cValue)
+})
+
+const str = ['Abhi','Aman','Ram',23,321];
+str.forEach(function(cValue,idx,array){
+    // array[idx] = cValue.toUpperCase;
+    array[idx] = cValue.toString;
+
+});
+//console.log(str);
+// for(let i=0;i<str.length;i++){
+// console.log(str[i]);
+// }
+
+
+const fruits = ['apple', 'banana', 'cherry', 'date', 'fig'];
+
+const slicedFruits = fruits.slice(1, 4);
+//console.log(slicedFruits);
+
+//const spliceElement = fruits.splice(2,3);
+const spliceElement = fruits.splice(2,0,'blue','red');
+//console.log(fruits);
+
+
+//Task -> 
+
+let a = {
+    name: 'abhi',
+    age: 10
+};
+let b = {
+    name: 'ravi',
+    age: 15
+};
+let c = {
+    name: 'akash',
+    age: 20
+};
+
+let arr1 = [];
+arr1.push(a);
+arr1.push(b);
+arr1.push(c);
+let arr2 = [];
+
+let res = arr.filter(arr1 => arr1.age%2 === 0);
+// arr2.push(res);
+// console.log(arr2);
+
+// console.log(res);
+
+const persons = [
+    { name: 'Alice', age: 10 },
+    { name: 'Bob', age: 15 },
+    { name: 'Charlie', age: 20 }
+];
+
+const personsWithEvenAge = persons.filter((cValue,idx,arr) => cValue.age % 2 === 0);
+
+//console.log(personsWithEvenAge[0]);
+
+
+//Task2 =>
+
+const productList = [
+    { product: 'Shoes', price: 1200 },
+    { product: 'Jeans', price: 1500 },
+    { product: 'Shirt', price: 700 }
+];
+
+//console.log(productList);
+
+let finalPriceOfProduct = productList.map((cValue) => {
+    return {product: cValue.product ,price: cValue.price + 500};
+})
+
+// console.log(finalPriceOfProduct[0]);
+// for(let i=0;i<finalPriceOfProduct.length;i++){
+//     console.log(finalPriceOfProduct[i]);
+// }
+
+let priceOnly = [];
+for(let i =0;i <finalPriceOfProduct.length;i++){
+priceOnly.push(finalPriceOfProduct[i].price);
 
 }
 
- mul(1,2,3,4,5);
-// console.log(multiply);
+console.log(priceOnly);
+let total = priceOnly.reduce(sumTotal);
 
-function print(...val) {
-    for (let i in val) {
-        console.log(val[i]);
-    }
+function sumTotal(acc,cValue){
+    return acc+cValue;
 }
 
-let val = ['a', 'b', { a: 10 }, 1, 2, 3, 4];
-print(...val);
+console.log("total price = "+total);
+
+
+const totalPrice2 = finalPriceOfProduct.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue.price;
+  }, 0);
+
+  console.log(totalPrice2)
+
+
 
 
 
